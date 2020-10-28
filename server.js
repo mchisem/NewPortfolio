c3f0777c578f0e4ade4072df69f02b16479faf79
Maya Chisem <chisem.maya@gmail.com>
2020-10-28 11:38:31 -0700

    adding server.js file

---
+++ server.js
@@ -0,0 +1,22 @@
+const compression = require("compression");
+const express = require("express");
+
+const PORT = process.env.PORT || 9001;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+app.listen(PORT, () => {
+    console.log(
+      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+      PORT,
+      PORT
+    );
+  });
+
+// compressed compression function
+app;function shouldCompress(s,e){return!s.headers["x-no-compression"]&&compression.filter(s,e)}app.use(compression({filter:shouldCompress}));
+
